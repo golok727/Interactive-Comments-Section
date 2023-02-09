5f93158be9835ab9c706164982ae8af65669390d
Radha743 <worldofnara777@gmail.com>
2023-02-09 13:26:29 +0530

    Js start

--- src/radhe.js
+++ src/radhe.js
@@ -1,1 +1,10 @@
 console.log("Radhey Shyam");
+(async () => {
+	const state = await fetchComments();
+	console.log(state);
+	async function fetchComments() {
+		const res = await fetch("./data.json");
+		const data = await res.json();
+		return data;
+	}
+})();
